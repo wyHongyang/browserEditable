9ef40ba9292dc5cbc5c75c3763ee7fd21195c289
hzhongyang <hzhongyang@HIH-D-5652.hz.ntes.domain>
2015-07-28 16:58:49 +0800

    misc:提交main

--- js/main.js
+++ js/main.js
@@ -11,8 +11,22 @@
 	}
 
 	$('.btn-primary').on('click',function(e){
-		var url = $(e.target).closest('.g-bd').find('input[type="radio"]:checked').closest('.u-radio').data('content');
-		window.open(unescape(url));
+		var baseurl = $(e.target).closest('.g-bd').find('input[type="radio"]:checked').closest('.u-radio').data('content');
+//		window.open(unescape(baseurl));
+		chrome.tabs.create({
+			url:unescape(baseurl),
+			selected:true,
+		},function(tab){
+			var _tabID = tab.id;
+			chrome.tabs.onUpdated.addListener(function(_tabID , info) {
+			    if (info.status == "complete") {
+					chrome.browserAction.onClicked.addListener(function(tab) {  
+						chrome.tabs.executeScript(tab.id, {file: 'js/lib/jquery-1.11.3.min.js'});  
+						chrome.tabs.executeScript(tab.id, {file: 'js/content.js'});  
+					});  
+			    }
+			});
+		});
 	});
 	
 	render();
